f41cac3f911c7eea833c4f81f6a447ded0686ded
ForteScarlet <ForteScarlet@163.com>
2026-01-13 01:08:22 +0800

    INIT

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.kotlin.dsl.javaToolchains
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.sqldelight)
+}
+
+kotlin {
+    jvmToolchain(21)
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutinesSwing)
+            implementation(libs.sqldelight.jvm)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "love.forte.tools.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "love.forte.tools"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ composeApp/src/jvmMain/kotlin/love/forte/tools/Platform.kt
@@ -0,0 +1,7 @@
+package love.forte.tools
+
+class JVMPlatform {
+    val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+fun getPlatform() = JVMPlatform()
