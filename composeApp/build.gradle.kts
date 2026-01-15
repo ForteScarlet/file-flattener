b6e1a57f9cbbcf7c9b290b110ad6f58bf0364f50
ForteScarlet <ForteScarlet@163.com>
2026-01-15 14:04:10 +0800

    Remove unused dependency on `navigation-compose` in build script

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,9 +43,6 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.serialization.json)
-            // https://docs.kmpstudy.com/compose-navigation-routing.html
-            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.8.0-alpha10")
-
         }
 
         commonTest.dependencies {
